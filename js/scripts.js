564d4ae9ebd29abe55a961906b72c472551a6413
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-05 14:43:41 -0700

    push phrases into the array

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,29 @@
 function robo(userInput) {
 	let inputArray = []
 	for (let i = 0; i <= userInput; i ++){
-  inputArray.push(i);
+    const conclusion = i.toString();
+    if (conclusion.includes(3)){
+      inputArray.push("Won't you be my neighbor")
+    }
+    else if (conclusion.includes(2)){
+      inputArray.push("Boop!")
+    }
+    else if (conclusion.includes(1)){
+      inputArray.push("Beep!")
+    }
+    else {
+      inputArray.push(conclusion)
+    }
 	}
  return inputArray; 
-  
+ 
+ 
 }
+
+
+$(document).ready(function(){
+  $().submit(function(event){
+    event.preventDefault();
+
+  });
+});
